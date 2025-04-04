c3b29f5e6602b2b51110f7348b4b48c5a5ca8bc1
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2025-04-04 13:36:21 +0000

    initial gptq support; need to fix for mini models
    Signed-off-by: Joshua Rosenkranz <jmrosenk@us.ibm.com>

--- tests/models/test_decoders.py
+++ tests/models/test_decoders.py
@@ -1,3 +1,4 @@
+from fms.models.hf.utils import AutoConfig
 from fms.utils import tokenizers
 import pytest
 from fms.models import get_model
@@ -5,9 +6,14 @@ from fms.utils.generation import pad_input_ids
 import itertools
 import torch
 from aiu_fms_testing_utils.testing.validation import extract_validation_information, LogitsExtractorHook, GoldenTokenHook, capture_level_1_metrics, filter_failed_level_1_cases, load_validation_information, validate_level_0, top_k_loss_calculator
-from aiu_fms_testing_utils.utils import warmup_model, sample_sharegpt_requests, ids_for_prompt, _prepare_model_inputs_hook
+from aiu_fms_testing_utils.utils import warmup_model, sample_sharegpt_requests, ids_for_prompt
 from aiu_fms_testing_utils.utils.aiu_setup import dprint
 import os
+try:
+    from fms_mo.aiu_addons.gptq import gptq_aiu_adapter, gptq_aiu_linear
+    GPTQ_ENABLED = True
+except ImportError:
+    GPTQ_ENABLED = False
 
 ORIGINAL_HF_HOME = os.environ.get("HF_HOME", None)
 
@@ -77,6 +83,16 @@ def reset_compiler():
     else:
         os.environ['HF_HOME'] = ORIGINAL_HF_HOME
 
+def __maybe_get_gptq_kwargs(model_path):
+    if GPTQ_ENABLED:
+        config = AutoConfig.from_pretrained(model_path)
+        if hasattr(config, "quantization_config"):
+            group_size = config.quantization_config["group_size"]
+            desc_act = config.quantization_config["desc_act"]
+            gptq_kwargs = {"group_size": group_size, "desc_act": desc_act}
+            return gptq_kwargs
+    return None
+
 def __prepare_inputs(batch_size, seq_length, tokenizer, seed=0):
     prompts_and_sizes = sample_sharegpt_requests(SHARE_GPT_DATASET_PATH, batch_size, tokenizer, int(seq_length / 2), seq_length, seed)
     prompt_list = []
@@ -136,6 +152,8 @@ def test_common_shapes(model_path, batch_size, seq_length, max_new_tokens):
     else:
         model_path_kwargs = {"variant": model_path}
     
+    gptq_kwargs = __maybe_get_gptq_kwargs(model_path)
+    
     get_model_kwargs = {**model_path_kwargs, **micro_model_kwargs}
 
     tokenizer = tokenizers.get_tokenizer(model_path)
@@ -144,7 +162,8 @@ def test_common_shapes(model_path, batch_size, seq_length, max_new_tokens):
     model = get_model(
         device_type="cpu",
         fused_weights=False,
-        **get_model_kwargs
+        linear_config={**gptq_kwargs, "linear_type": "gptq_aiu"} if gptq_kwargs else None,
+        **get_model_kwargs,
     )
 
     model.eval()
@@ -156,6 +175,7 @@ def test_common_shapes(model_path, batch_size, seq_length, max_new_tokens):
         device_type="cpu",
         data_type=torch.float32,
         fused_weights=False,
+        linear_config={**gptq_kwargs, "linear_type": "gptq_cpu"} if gptq_kwargs else None,
         **get_model_kwargs
     )
 
@@ -274,74 +294,3 @@ def test_common_shapes(model_path, batch_size, seq_length, max_new_tokens):
         print("passed validation level 1")
     else:
         print("passed validation level 0")
-
-def test_warmup_multiple_shapes():
-    shapes = [
-        (1, 64, 8),
-        (2, 64, 8),
-        (1, 128, 24),
-    ]
-
-    reference_model = get_model(
-        architecture="hf_configured",
-        variant=GRANITE_3p2_8B_INSTRUCT,
-        device_type="cpu",
-        fused_weights=False,
-        nlayers=3
-    )
-
-    model = get_model(
-        architecture="hf_configured",
-        variant=GRANITE_3p2_8B_INSTRUCT,
-        device_type="cpu",
-        fused_weights=False,
-        nlayers=3
-    )
-
-    reference_model.load_state_dict(model.state_dict())
-
-    model.eval()
-    reference_model.eval()
-
-    torch.set_grad_enabled(False)
-    model.compile(backend="sendnn_decoder")
-    for bs, sl, mnt in shapes:
-        # prepare input_ids
-        prompt_list = []
-        for i in range(bs):
-            prompt_list.append(torch.randint(0, model.config.src_vocab_size, (sl - 2 * i,), dtype=torch.long))
-
-        input_ids, padding_kwargs = pad_input_ids(prompt_list, min_pad_length=sl)
-        # warmup aiu model
-        warmup_model(model, input_ids, mnt, **padding_kwargs)
-    
-    # perform 3 inference, making sure ordering does not affect things
-    for _ in range(3):
-        shapes.reverse()
-        for bs, sl, mnt in shapes:
-            prompt_list = []
-            for i in range(bs):
-                prompt_list.append(torch.randint(0, model.config.src_vocab_size, (sl - 2 * i,), dtype=torch.long))
-            input_ids, padding_kwargs = pad_input_ids(prompt_list, min_pad_length=sl)
-
-            cpu_validation_info = extract_validation_information(
-                reference_model,
-                input_ids,
-                mnt,
-                LogitsExtractorHook(),
-                attn_algorithm="math",
-                **padding_kwargs
-            )
-
-            aiu_validation_info = extract_validation_information(
-                model,
-                input_ids,
-                mnt,
-                None,
-                only_last_token=True,
-                **padding_kwargs
-            )
-
-            failed_responses = validate_level_0(aiu_validation_info.get_info("tokens"), cpu_validation_info.get_info("tokens"))
-
-            assert len(failed_responses) == 0
