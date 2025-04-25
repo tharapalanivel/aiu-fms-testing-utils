ad225d91e28676c03d627d9713476995ffc6b159
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2025-04-25 02:59:41 +0000

    fixed bug in test_model_expectations compilation in test_model_expectations; updated the models to be tested - granite-3.2-8b-instruct and llama-3.1-8b-instruct
    Signed-off-by: Joshua Rosenkranz <jmrosenk@us.ibm.com>

--- tests/models/test_model_expectations.py
+++ tests/models/test_model_expectations.py
@@ -1,6 +1,12 @@
 from fms.models import get_model
+from fms.utils.generation import pad_input_ids
+from fms.utils.tokenizers import get_tokenizer
 import pytest
+from aiu_fms_testing_utils.utils import sample_squad_v2_qa_requests
 import torch
+from aiu_fms_testing_utils.utils import (
+    ids_for_prompt,
+)
 
 from fms.testing._internal.model_test_suite import (
     ModelConsistencyTestSuite,
@@ -8,45 +14,68 @@ from fms.testing._internal.model_test_suite import (
 )
 import os
 
+os.environ["COMPILATION_MODE"] = "offline"
+
 if "HF_HOME" not in os.environ:
     os.environ["HF_HOME"] = "/tmp/models/hf_cache"
 
 model_dir = os.environ.get("FMS_TESTING_MODEL_DIR", "/tmp/models")
-LLAMA_194M = f"{model_dir}/llama-194m"
-GRANITE_7B_BASE = f"{model_dir}/granite-7b-base"
-GRANITE_8B_CODE_BASE = f"{model_dir}/granite-8b-code-base"
-GRANITE_3_8B_CODE_BASE = f"{model_dir}/granite-3-8b-base"
+LLAMA_3p1_8B_INSTRUCT = "meta-llama/Llama-3.1-8B-Instruct"
+GRANITE_3p2_8B_INSTRUCT = "ibm-granite/granite-3.2-8b-instruct"
+ROBERTA_SQUAD_v2 = "deepset/roberta-base-squad2"
 
-models = [LLAMA_194M, GRANITE_7B_BASE, GRANITE_8B_CODE_BASE, GRANITE_3_8B_CODE_BASE]
-mini_models = {LLAMA_194M, GRANITE_7B_BASE, GRANITE_8B_CODE_BASE, GRANITE_3_8B_CODE_BASE}
 
-class AIUModelFixtureMixin(ModelFixtureMixin):
-
+class AIUMicroModelFixtureMixin(ModelFixtureMixin):
     @pytest.fixture(scope="class", autouse=True)
     def uninitialized_model(self, model_id):
-        if model_id in mini_models:
-            get_model_kwargs = {"architecture": "hf_configured", "nlayers": 3}
-        else:
-            get_model_kwargs = {"architecture": "hf_pretrained"}
-
         aiu_model = get_model(
+            architecture="hf_configured",
             variant=model_id,
             device_type="cpu",
-            unfuse_weights=True,
-            **get_model_kwargs
+            fused_weights=False,
+            data_type=torch.float16,
+            nlayers=3,
         )
-        torch.compile(aiu_model, backend="sendnn")
+        aiu_model.eval()
+        torch.set_grad_enabled(False)
+        aiu_model.compile(backend="sendnn")
         return aiu_model
 
-class TestAIUModels(
-    ModelConsistencyTestSuite,
-    AIUModelFixtureMixin,
-):
 
+class AIUModelFixtureMixin(ModelFixtureMixin):
+    @pytest.fixture(scope="class", autouse=True)
+    def uninitialized_model(self, model_id):
+        return None
+
+    @pytest.fixture(scope="class", autouse=True)
+    def model(self, uninitialized_model, model_id):
+        aiu_model = get_model(
+            architecture="hf_pretrained",
+            variant=model_id,
+            device_type="cpu",
+            fused_weights=False,
+            data_type=torch.float16,
+        )
+        aiu_model.eval()
+        torch.set_grad_enabled(False)
+        aiu_model.compile(backend="sendnn")
+        return aiu_model
+
+
+micro_decoder_models = [LLAMA_3p1_8B_INSTRUCT, GRANITE_3p2_8B_INSTRUCT]
+
+
+class TestAIUMicroDecoderModels(
+    ModelConsistencyTestSuite,
+    AIUMicroModelFixtureMixin,
+):
     # x is the main parameter for this model which is the input tensor
     _get_signature_params = ["x"]
+    _get_signature_input_ids, _get_signature_optional_params = pad_input_ids(
+        [torch.arange(start=5, end=65, dtype=torch.int64)], min_pad_length=64
+    )
 
-    @pytest.fixture(scope="class", autouse=True, params=models)
+    @pytest.fixture(scope="class", autouse=True, params=micro_decoder_models)
     def model_id(self, request):
         return request.param
 
@@ -54,25 +83,28 @@ class TestAIUModels(
         pytest.skip("All AIU models are already unfused")
 
 
-ROBERTA_SQUAD_v2 = "deepset/roberta-base-squad2"
 tuple_output_models = [ROBERTA_SQUAD_v2]
+
 
 class TestAIUModelsTupleOutput(
     ModelConsistencyTestSuite,
     AIUModelFixtureMixin,
 ):
-    
     # x is the main parameter for this model which is the input tensor
     _get_signature_params = ["x"]
+    _get_signature_input_ids, _get_signature_optional_params = pad_input_ids(
+        [torch.arange(start=5, end=65, dtype=torch.int64)],
+        min_pad_length=64,
+        is_causal_mask=False,
+    )
 
     @pytest.fixture(scope="class", autouse=True, params=tuple_output_models)
     def model_id(self, request):
         return request.param
-    
+
     @staticmethod
     def _get_signature_logits_getter_fn(f_out) -> torch.Tensor:
         return torch.cat([f_out[0], f_out[1]], dim=-1)
-    
+
     def test_model_unfused(self, model, signature):
         pytest.skip("All AIU models are already unfused")
-    
