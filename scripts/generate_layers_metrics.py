54017ac714228bd1bec35e1277e4a63af72b41f3
Flavia Beo <flavia.beo@ibm.com>
2025-07-01 20:32:18 -0300

    Adds python logger
    Signed-off-by: Flavia Beo <flavia.beo@ibm.com>

--- scripts/generate_layers_metrics.py
+++ scripts/generate_layers_metrics.py
@@ -17,6 +17,9 @@ from aiu_fms_testing_utils.utils import (
 )
 from aiu_fms_testing_utils.utils.aiu_setup import dprint
 
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 ORIGINAL_HF_HOME = os.environ.get("HF_HOME", None)
@@ -69,7 +72,6 @@ def __prepare_inputs(batch_size, seq_length, tokenizer, seed=0):
         seq_length,
         seed,
     )
-    print(prompts_and_sizes)
     ## TODO: for each prompt 
     prompt_list = []
     for prompt, _ in prompts_and_sizes:
@@ -89,8 +91,6 @@ def __infer_layer(warmup, model, max_len, device,
     ids, pad_input_ids = prompts
 
     if "cuda" in device:
-        print("cuda prompts")
-        print(len(prompts))
         ids = ids.to("cuda")
     
     if hasattr(model.config, "ntk_scaling") and model.config.ntk_scaling:
@@ -99,26 +99,27 @@ def __infer_layer(warmup, model, max_len, device,
         # without ntk scaling, extending the seq length too far gives bogus results.
         max_seq_len = model.config.max_expected_seq_len
 
-    result = generate(
-        model,
-        ids,
-        max_new_tokens=max_new_tokens,
-        use_cache=use_cache,
-        do_sample=do_sample,
-        max_seq_len=max_seq_len,
-        timing="e2e",
-        eos_token_id=None,
-        contiguous_cache=True,
-        extra_kwargs={},
-    )
-    result, timings = result
-    dprint(f"E2E timing information: {timings[0]:.3f}s")
+    with torch.no_grad():
+        result = generate(
+            model,
+            ids,
+            max_new_tokens=max_new_tokens,
+            use_cache=use_cache,
+            do_sample=do_sample,
+            max_seq_len=max_seq_len,
+            timing="e2e",
+            eos_token_id=None,
+            contiguous_cache=True,
+            extra_kwargs={},
+        )
+        result, timings = result
+    logger.info(f"E2E timing information: {timings[0]:.3f}s")
     if len(result.shape) == 1:
         result = result.unsqueeze(0)
 
     if not warmup:
         for i in range(result.shape[0]):
-            print(result[i])
+            logger.debug(result[i])
 
 def __register_call_layers(model, batch_size, device, seq_length, max_new_tokens, tokenizer):
     layer_stack = []
@@ -145,12 +146,10 @@ def __register_call_layers(model, batch_size, device, seq_length, max_new_tokens
         original_forward = layer.forward
 
         def safe_forward(*args, **kwargs):
-            #print("In safe forward")
             try:
-                #print("No Error")
                 return original_forward(*args, **kwargs)
             except (RuntimeError,TypeError) as e:
-                print(f"Error in {layer.__class__.__name__}: {e}")
+                logger.error(f"Error in {layer.__class__.__name__}: {e}")
                 return torch.zeros_like(args[0]) if args else None
         layer.forward = safe_forward
         
@@ -166,17 +165,15 @@ def __register_call_layers(model, batch_size, device, seq_length, max_new_tokens
         if module.parameters() == None: return
         param_size = sum(p.numel() for p in module.parameters() if p.requires_grad)
         param_size_str = f"{param_size:,}" if param_size > 0 else "--"
-        print(f"DEBUG TOOL {prefix}├─{layer_name}() -> {module.__class__.__name__} : | Input(arg): {input_shape_str} | {input_type} | Params: {param_size_str}")
+        logger.info(f"DEBUG TOOL {prefix}├─{layer_name}() -> {module.__class__.__name__} : | Input(arg): {input_shape_str} | {input_type} | Params: {param_size_str}")
         wrap_forward(module)
         # save input for later use with outputs
         module._debug_input = input 
 
     def post_hook_fn(module, input, output):
-        print("post_hook_fn")
         layer_name = module_name.get(module, 0)
         # Save inputs and outputs
         if hasattr(module, '_debug_input'):
-            print(output)
             layer_stack.append((layer_name, output))
             # Clean up
             delattr(module, '_debug_input')
@@ -195,20 +192,17 @@ def __register_call_layers(model, batch_size, device, seq_length, max_new_tokens
         hook.remove()
 
     pt_compile_model_time = time.time() - pt_compile_model_time
-    dprint(f"PT compile complete, took {pt_compile_model_time:.3f}s")
+    logger.info(f"PT compile complete, took {pt_compile_model_time:.3f}s")
 
     return layer_stack
 
 def write_csv(l, path, metric):
-    print("saving file")
     with open(path, 'w') as f:
         f.write(f'{metric}\n')
         if not type(l) is float:
-            print("saving list")
             for t in l:
                 f.write(f"{t}\n")
         else:
-            print("saving float")
             f.write(f"{l}\n")
         f.close()
 
@@ -272,7 +266,7 @@ def generate_layers_metrics(model_path, batch_size, seq_length, max_new_tokens):
         abs_diff = None
         for cpu_layer, cpu_output in layer_stack_cpu:
             if cpu_layer == layer:
-                print("CPU Layer {} GPU Layer {}".format(cpu_layer, layer))
+                logger.info("CPU Layer {} GPU Layer {}".format(cpu_layer, layer))
 
                 if not type(cuda_output) is tuple:
                     tensor_cuda_out = cuda_output
@@ -282,26 +276,22 @@ def generate_layers_metrics(model_path, batch_size, seq_length, max_new_tokens):
                     tensor_cpu_out = convert_tensor(cpu_output)
                 else:
                     tensor_cpu_out = cpu_output.to('cuda')
-                print("tensor converted... get torch abs diff")
+                logger.debug("tensor converted... get torch abs diff")
                 abs_diff = torch.abs(tensor_cpu_out - tensor_cuda_out).flatten().tolist()
-                print("abs_diff calculated")
+                logger.debug("abs_diff calculated")
                 cos = nn.CosineSimilarity(dim=1)
                 cos_sim = cos(tensor_cpu_out, tensor_cuda_out)
-                print(cos_sim)
-
-        absolute_differences.append(abs_diff)
-        print("abs_diff list appended")
-        print(len(absolute_differences))
+                logger.debug(cos_sim)
 
         prefix = get_default_validation_prefix(model_path, max_new_token, batch_size, 0, 'float16')
         layer_name = str(layer).replace('[','').replace(']', '')
 
-        print("saving files")
+        logger.debug("saving files")
         write_csv(abs_diff, os.path.join(output_dir, f"{prefix}--{layer_name}.abs_diff.csv"), "abs_diff")
         write_csv(cos_sim, os.path.join(output_dir, f"{prefix}--{layer_name}.cos_sim.csv"), "cos_sim")
         
-    print(f"Completed {model_path} layers' metrics generation")
+    logger.info(f"Completed {model_path} layers' metrics generation")
 
 for model_id, batch_size, sequence_length, max_new_token in common_shapes:
-    print("testing ", "model_id-", model_id, ", max_new_tokens-", max_new_token, ", batch_size-",batch_size, ", seq_length-",sequence_length)
+    logger.info("testing ", "model_id-", model_id, ", max_new_tokens-", max_new_token, ", batch_size-",batch_size, ", seq_length-",sequence_length)
     generate_layers_metrics(model_path=model_id, batch_size=batch_size, seq_length=sequence_length, max_new_tokens=max_new_token)
