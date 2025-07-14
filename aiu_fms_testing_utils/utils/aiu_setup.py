4d2513c0822a85974f30006480326ea960074ccc
Andrea Fasoli <andrea.fasoli@ibm.com>
2025-07-14 16:42:31 +0000

    Gate cache initialization
    Signed-off-by: Andrea Fasoli <andrea.fasoli@ibm.com>

--- aiu_fms_testing_utils/utils/aiu_setup.py
+++ aiu_fms_testing_utils/utils/aiu_setup.py
@@ -77,39 +77,36 @@ def aiu_dist_setup(rank, world_size, local_rank=-0, local_size=-1, verbose=False
 def set_aiu_env_vars(args: argparse.Namespace) -> None:
     """Set necessary environment variables for AIU"""
 
-    if not args.compile_dynamic:
-        _target_cache_size = max(
-            int(args.max_new_tokens * 2),
-            int(args.min_pad_length * 2.5),
-            int(args.fixed_prompt_length * 2.5),
-        )
-        _prompt_size = max(int(args.min_pad_length), int(args.fixed_prompt_length))
-        if hasattr(torch._dynamo.config, "accumulated_cache_size_limit"):
-            if _target_cache_size > torch._dynamo.config.accumulated_cache_size_limit:
-                _prev = torch._dynamo.config.accumulated_cache_size_limit
-                torch._dynamo.config.accumulated_cache_size_limit = _target_cache_size
-                dprint(
-                    "NOTICE: Adjusting torch._dynamo.config.accumulated_cache_size_limit "
-                    f"from {_prev} to {torch._dynamo.config.accumulated_cache_size_limit} "
-                    f"to accomodate prompt size of {_prompt_size} and decode tokens of "
-                    f"{args.max_new_tokens}"
-                )
-
-        if _target_cache_size > torch._dynamo.config.cache_size_limit:
-            _prev = torch._dynamo.config.cache_size_limit
-            torch._dynamo.config.cache_size_limit = _target_cache_size
-            dprint(
-                f"NOTICE: Adjusting torch._dynamo.config.cache_size_limit from {_prev} to "
-                f"{torch._dynamo.config.cache_size_limit} to accomodate prompt size of "
-                f"{_prompt_size} and decode tokens of {args.max_new_tokens}"
-            )
-
-        torch._dynamo.config.assume_static_by_default = True
-        torch._dynamo.config.automatic_dynamic_shapes = False
-
-    # os.environ.setdefault("DTCOMPILER_KEEP_EXPORT", "true")  # CONFIRM IF THIS IS NEEDE
-
     if not args.is_encoder:
+        if not args.compile_dynamic:
+            _target_cache_size = max(
+                int(args.max_new_tokens * 2),
+                int(args.min_pad_length * 2.5),
+                int(args.fixed_prompt_length * 2.5),
+            )
+            _prompt_size = max(int(args.min_pad_length), int(args.fixed_prompt_length))
+            if hasattr(torch._dynamo.config, "accumulated_cache_size_limit"):
+                if _target_cache_size > torch._dynamo.config.accumulated_cache_size_limit:
+                    _prev = torch._dynamo.config.accumulated_cache_size_limit
+                    torch._dynamo.config.accumulated_cache_size_limit = _target_cache_size
+                    dprint(
+                        "NOTICE: Adjusting torch._dynamo.config.accumulated_cache_size_limit "
+                        f"from {_prev} to {torch._dynamo.config.accumulated_cache_size_limit} "
+                        f"to accomodate prompt size of {_prompt_size} and decode tokens of "
+                        f"{args.max_new_tokens}"
+                    )
+            if _target_cache_size > torch._dynamo.config.cache_size_limit:
+                _prev = torch._dynamo.config.cache_size_limit
+                torch._dynamo.config.cache_size_limit = _target_cache_size
+                dprint(
+                    f"NOTICE: Adjusting torch._dynamo.config.cache_size_limit from {_prev} to "
+                    f"{torch._dynamo.config.cache_size_limit} to accomodate prompt size of "
+                    f"{_prompt_size} and decode tokens of {args.max_new_tokens}"
+                )
+            torch._dynamo.config.assume_static_by_default = True
+            torch._dynamo.config.automatic_dynamic_shapes = False
+
+        # os.environ.setdefault("DTCOMPILER_KEEP_EXPORT", "true")
         os.environ.setdefault("COMPILATION_MODE", "offline_decoder")
 
     if args.device_type == "aiu-senulator":
